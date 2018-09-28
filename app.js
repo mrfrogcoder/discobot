7ec5ac3765d772314f34448cadbc450876d7773e
Van Aldrin Sarzate <van.sarzate@flexisourceit.com.au>
2018-09-28 13:22:16 +0800

    Update

--- app.js
+++ app.js
@@ -29,7 +29,7 @@
 // });
 // client.login(process.env.BOT_TOKEY);
 // Extract the required classes from the discord.js module
-const { Client, Attachment } = require('discord.js');
+const { Client, Attachment, RichEmbed } = require('discord.js');
 
 // Create an instance of a Discord client
 const client = new Client();
@@ -49,6 +49,36 @@ client.on('message', message => {
     const attachment = new Attachment('https://i.imgur.com/w3duR07.png');
     // Send the attachment in the message channel
     message.channel.send(attachment);
+  }
+
+  if (message.content === 'how to embed') {
+    // We can create embeds using the MessageEmbed constructor
+    // Read more about all that you can do with the constructor
+    // over at https://discord.js.org/#/docs/main/stable/class/RichEmbed
+    const embed = new RichEmbed()
+      // Set the title of the field
+      .setTitle('A slick little embed')
+      // Set the color of the embed
+      .setColor(0xff0000)
+      // Set the main content of the embed
+      .setDescription('Hello, this is a slick embed!');
+    // Send the embed to the same channel as the message
+    message.channel.send(embed);
+  }
+
+  if (message.content === '!memes') {
+    // Get the buffer from the 'memes.txt', assuming that the file exists
+    const buffer = fs.readFileSync('./memes.txt');
+
+    /**
+     * Create the attachment using Attachment,
+     * overwritting the default file name to 'memes.txt'
+     * Read more about it over at
+     * http://discord.js.org/#/docs/main/stable/class/Attachment
+     */
+    const attachment = new Attachment(buffer, 'memes.txt');
+    // Send the attachment in the message channel with a content
+    message.channel.send(`${message.author}, here are your memes!`, attachment);
   }
 });
 
