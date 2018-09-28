427434bdbefba02e23af5d810985e65597b5e789
Van Aldrin Sarzate <vanaldrinsarzate@gmail.com>
2018-09-28 10:02:03 +0800

    create bot.js

---
+++ bot.js
@@ -0,0 +1,53 @@
+var Discord = require('discord.io');
+var logger = require('winston');
+
+
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, {
+  colorize: true
+});
+logger.level = 'debug';
+
+// Initialize Discord Bot
+var bot = new Discord.Client({
+  token: auth.token,
+  autorun: true
+});
+
+bot.on('ready', function(evt) {
+  logger.info('Connected');
+  logger.info('Logged in as: ');
+  logger.info(bot.username + ' - (' + bot.id + ')');
+});
+
+bot.on('message', function(user, userID, channelID, message, evt) {
+  // Our bot needs to know if it needs to execute a command
+  // for this script it will listen for messages that will start with `!`
+  if (message.substring(0, 1) == '!') {
+    var args = message.substring(1).split(' ');
+    var cmd = args[0];
+
+    args = args.splice(1);
+
+    switch (cmd) {
+      // !ping
+      case 'ping':
+        bot.sendMessage({ to: channelID, message: 'Pong!' });
+        break;
+      case 'shiny':
+        bot.sendMessage({ to: channelID, message: 'Shiny Alden you like? :p' });
+        break;
+      case 'xcoord':
+        bot.sendMessage({
+          to: channelID,
+          message:
+            '14.650817, 121.07525, Globe Store UP TOWN, Quezon City, Metro Manila, Philippine\n14.653978, 121.032959, Globe Store Trinoma, Quezon City, Metro Manila, Philippine\n'
+        });
+        break;
+
+      default:
+        bot.sendMessage({ to: channelID, message: 'Unknown command.' });
+    }
+  }
+});
